f3b25a5fc8841cba2a0be7e3fb16597f8b5de925
Mohd Abdul Mannan <ma24042000@gmail.com>
2024-02-23 12:29:03 +0530

    1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 export function Header() {
     return <>
-        <div className="flex justify-space
-         p-1 pb-2">
+        <div className="flex justify-between p-1 pb-2">
             <div className="text-white"> <h1 className="text-xl pl-5">Abdul Mannan</h1></div>
             <div className="flex justify-between pr-5">
                 <div> <button className="bg-white rounded-lg">Resume</button></div>
