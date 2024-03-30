baa7bab351c69679044348af5bd80229cb566079
Mohd Abdul Mannan <ma24042000@gmail.com>
2024-03-30 06:32:27 +0530

    update 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,10 @@ export function Header() {
         <div className="flex justify-between p-1 pb-2">
             <div className="text-white"> <h1 className="text-xl pl-5">Abdul Mannan</h1></div>
             <div className="flex justify-between pr-5">
-                <div> <button className="bg-white rounded-lg">Resume</button></div>
+                <div> <button className="bg-white rounded-lg" section="#">Resume</button></div>
                 <div> <button className="bg-white rounded-lg">Projects</button></div>
-                <div> <button className="bg-white rounded-lg">Contact</button></div>
+                <div> <button className="bg-white rounded-lg">Internships</button></div>
+                <div> <button className="bg-white rounded-lg"> Contact me</button></div>
             </div>
 
         </div>
